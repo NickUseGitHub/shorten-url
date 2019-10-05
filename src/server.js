173053075cc5a22f403526a546f28b8fee18be58
Nick <chaiwat@tencent.co.th>
2019-10-05 22:22:57 +0700

    [universal-app] create simple ssr

---
+++ src/server.js
@@ -0,0 +1,12 @@
+export default function reactHandler(ctx) {
+  ctx.body = `
+  <html>
+    <head>
+      <title>Universal App</title>
+    </head>
+    <body>
+      Hello World
+    </body>
+  </html>
+  `
+}
