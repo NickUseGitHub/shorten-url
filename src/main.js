39b597136303c8cad90263c40635e00be191a673
Nick <chaiwat@tencent.co.th>
2019-10-05 20:40:49 +0700

    [eslint-prettier] refactor code to es6

--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-import Koa from 'koa';
+import Koa from 'koa'
 import helloString from './helloworldString'
 
-const app = new Koa();
+const app = new Koa()
 
 // logger
 
 app.use(async (ctx, next) => {
-  await next();
-  const rt = ctx.response.get('X-Response-Time');
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
-});
+  await next()
+  const rt = ctx.response.get('X-Response-Time')
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`)
+})
 
 // x-response-time
 
 app.use(async (ctx, next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  ctx.set('X-Response-Time', `${ms}ms`);
-});
+  const start = Date.now()
+  await next()
+  const ms = Date.now() - start
+  ctx.set('X-Response-Time', `${ms}ms`)
+})
 
 // response
 
 app.use(async ctx => {
-  ctx.body = helloString;
-});
+  ctx.body = helloString
+})
 
-app.listen(3000);
+app.listen(3000)
