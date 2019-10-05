a36ae5881b4dcf27e05c2060bbe3634978f0170b
Nick <chaiwat@tencent.co.th>
2019-10-05 21:00:53 +0700

    [create-koa-route] apply koa-router

--- src/main.js
+++ src/main.js
@@ -1,31 +1,17 @@
 import Koa from 'koa'
+import Router from 'koa-router'
 import helloString from './helloworldString'
 
 const app = new Koa()
 const port = 3000
 
-// logger
+const router = new Router()
 
-app.use(async (ctx, next) => {
-  await next()
-  const rt = ctx.response.get('X-Response-Time')
-  console.log(`${ctx.method} ${ctx.url} - ${rt}`)
-})
-
-// x-response-time
-
-app.use(async (ctx, next) => {
-  const start = Date.now()
-  await next()
-  const ms = Date.now() - start
-  ctx.set('X-Response-Time', `${ms}ms`)
-})
-
-// response
-
-app.use(async ctx => {
+router.get('/', ctx => {
   ctx.body = helloString
 })
+
+app.use(router.routes()).use(router.allowedMethods())
 
 console.log(`app is now listen on port: ${port}`)
 app.listen(port)
