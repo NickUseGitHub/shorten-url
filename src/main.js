b923aed7d6128ff410cc8027f2e1b94c04547406
Nick <chaiwat@tencent.co.th>
2019-10-05 21:36:27 +0700

    [create-koa-route] validate route shorturl

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import Koa from 'koa'
 import Router from 'koa-router'
+import querystring from 'querystring'
 
 const app = new Koa()
 const port = 3000
 
 const router = new Router()
 
-router.get('/shorturl', ctx => {
-  ctx.set('Content-Type: application/json')
+async function validateQueryString(ctx, next) {
+  const parsedUrl = querystring.parse(ctx.querystring)
+
+  if (!parsedUrl.url) {
+    ctx.throw(400, 'querystring url is required')
+    return
+  }
+  next()
+}
+
+router.get('/shorturl', validateQueryString, ctx => {
+  ctx.set('Content-Type', 'application/javascript')
   ctx.body = JSON.stringify({
     data: {
       shortUrl: 'shorturl',
