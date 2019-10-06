a44d0b571a3d1775bee9d6f7fe5e8c840b9b204a
Nick <chaiwat@tencent.co.th>
2019-10-06 15:30:53 +0700

    [shorten-url] refactor connector code

--- src/connectors/index.js
+++ src/connectors/index.js
@@ -1,21 +1,22 @@
 import redis from 'redis'
+import RedisDb from './redisDb'
 
 export default function initialConnect() {
   const redisClient = redis.createClient()
 
   redisClient.on('error', function(err) {
-    console.log('Error ' + err)
+    console.log('Redis Error ' + err)
   })
 
   redisClient.on('connect', function(err) {
     if (err) {
-      console.log('Error ' + err)
+      console.log('Redis connection Error' + err)
     }
 
     console.log('Redis is now connected on ported 6379')
   })
 
   return {
-    redisClient,
+    redisDB: new RedisDb({ client: redisClient }),
   }
 }
